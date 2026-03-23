80bd82dabd317d6e3302b89a361a6a27e8723632
sharqtechuz <sharqtechuz@gmail.com>
2026-03-23 16:10:08 +0500

    update bot

--- src/bot/handlers/start.js
+++
@@ -1,9 +0,0 @@
-const { mainMenu } = require('../keyboards');
-const { trackUser } = require('./stats');
-
-module.exports = function startHandler(bot) {
-  bot.start((ctx) => {
-    trackUser(ctx.from.id);
-    ctx.reply('👋 SharqTech botiga xush kelibsiz!', mainMenu);
-  });
-};
